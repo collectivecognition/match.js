07bac0cc05380a5b263e81c3123f53f9cc6ab627
William Blanchette <wb@collectivecognition.com>
2013-05-18 19:51:15 -0600

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-match.js
+match.jsh
 ========
 
-A reimplementation of Rob Pike's simplified regular expression matcher from Beautiful Code.
+A reimplementation of Rob Pike's simplified regular expression matcher from (Beautiful Code)[http://shop.oreilly.com/product/9780596510046.do] by O'Reilly Media (a great book, by the way) in Javascript.
+
+My implementation sticks pretty close to the original with the following changes (more or less):
+
+- Wrapped all functionality in the main function, camel-cased function names and some other minor tidying
+- Swapped out integer return values for booleans, because sometimes truthy just isn't truthy enough
+- Replaced C pointer fancyness with string subscript notation and judicious use of String.slice as required
+- End-of-string conditions are now based on string length, rather than an EOS character being present
+- Added a check for empty inputs
---
+++ match.js
@@ -0,0 +1,40 @@
+
+// c 	Matches any literal character c
+// .	Matches any single character
+// ^	Matches the beginning of the input string
+// $	Matches the end of the input string
+// *	Matches zero or more occurences of the previous character
+
+var match = function(regexp, text){
+	var matchHere = function(regexp, text){
+		if(regexp.length === 0) return true;
+		if(regexp[1] === "*") return matchStar(regexp[0], regexp.slice(2), text);
+		if(regexp[0] === "$" && regexp.length === 1) return text.length === 0;
+		if(text.length > 0 && (regexp[0] === "." || regexp[0] === text[0])) return matchHere(regexp.slice(1), text.slice(1));
+		return false;
+	}
+	var matchStar = function(c, regexp, text){
+		do{
+			if(matchHere(regexp, text)) return true;
+		}while(text.length > 0 && (text[1] === c || c === "."));
+	}
+	if(!regexp || !text) return false;
+	if(regexp[0] === "^") return matchHere(regexp.slice(1), text);
+	do{
+		if(matchHere(regexp, text)) return true;
+		text = text.slice(1);
+	}while(text.length > 0);
+	return false;
+}
+
+// Tests
+
+console.log(match("abcd", "abcd"));			// True
+console.log(match("abcd", "zabcdz"));		// True
+console.log(match("^abcd", "abcdef"));		// True
+console.log(match("^abcd", "zabcdef"));		// False
+console.log(match("abcd$", "abcd"));		// True
+console.log(match("abcd$", "abcdz"));		// False
+console.log(match("a..d*", "abcddddf"));	// True
+console.log(match("a..d*", "azza"));		// True
+console.log(match("a..d*", "bsddddd"));		// False
